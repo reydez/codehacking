a3e8b61ccc7eafbe1a77ebf7c760be2359505450
Rodrigo Reyes <icyreyeslod@gmail.com>
2019-01-25 01:18:29 -0600

    users with photos is working now

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -26,8 +26,8 @@
         </div>
 
         <div class="form-group">
-            {!! Form::label('file', 'File:') !!}
-            {!! Form::file('file', ['class'=>'form-control']) !!}
+            {!! Form::label('photo_id', 'Photo:') !!}
+            {!! Form::file('photo_id', ['class'=>'form-control']) !!}
         </div>
 
         <div class="form-group">
